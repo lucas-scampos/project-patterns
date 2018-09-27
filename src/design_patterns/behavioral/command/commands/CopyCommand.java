ae95bf088a4d564ba6ee4bf34c9ff8eafdb0df96
Lucas Campos <lucasaraujo@zgsolucoes.com.br>
2018-09-27 09:16:57 -0300

    Command design pattern

---
+++ src/design_patterns/behavioral/command/commands/CopyCommand.java
@@ -0,0 +1,16 @@
+package design_patterns.behavioral.command.commands;
+
+import design_patterns.behavioral.command.editor.Editor;
+
+public class CopyCommand extends Command {
+
+    public CopyCommand(Editor editor) {
+		super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
